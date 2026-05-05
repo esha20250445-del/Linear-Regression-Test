cf4dc6fc08e8c9527808ea7d14e91532ccf96feb
esha20250445-del <esha20250445@miis.ae>
2026-05-05 11:12:49 +0400

    Create data.py

---
+++ data.py
@@ -0,0 +1,9 @@
+import streamlit as st
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+df=pd.read_csv ("data.csv")
+X=df[["HoursStudied"]]
+Y=df[["ExamScore"]]
+X_train, X_test, Y_train, Y_test=train_test_split(X, Y, test_size=0.2, random_state=42)
+model=LinearRegression()
