6462f5226a40690072430f094989b89ee25a7c90
esha20250445-del <esha20250445@miis.ae>
2026-05-05 11:13:56 +0400

    Update data.py

--- data.py
+++ data.py
@@ -7,3 +7,4 @@ X=df[["HoursStudied"]]
 Y=df[["ExamScore"]]
 X_train, X_test, Y_train, Y_test=train_test_split(X, Y, test_size=0.2, random_state=42)
 model=LinearRegression()
+model.fit(X_train, Y_train)
